cc1045158f89929c636cb0ef59c0da858a9a7b78
Or Inbar <orinbar21@gmail.com>
2026-02-06 10:34:38 +0200

    java linter + ci versions

--- src/main/java/com/ido/HelloWorld.java
+++ src/main/java/com/ido/HelloWorld.java
@@ -3,9 +3,18 @@ package com.ido;
 /**
  * Hello world!
  */
- 
-public class HelloWorld {
-    public static void main(String[] args) {
+public final class HelloWorld {
+
+    private HelloWorld() {
+        // Utility class - prevent instantiation
+    }
+
+    /**
+     * Main entry point.
+     *
+     * @param args command line arguments
+     */
+    public static void main(final String[] args) {
         System.out.println("Hello World! I'm OrInbar");
     }
 }
